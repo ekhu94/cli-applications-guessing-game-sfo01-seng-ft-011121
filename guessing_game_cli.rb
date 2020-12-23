379e0c0b3e04592c0ea041af39498e9ef4215a1f
Erik Huang <erikhuangdrums@gmail.com>
2020-12-23 18:55:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,8 @@
 def run_guessing_game
   answer = rand(1..6)
-  answer
+  print "Guess a number between 1 and 6"
+  input = gets
+  return "Guess: #{input}, Answer: #{answer}"
 end
 
 puts run_guessing_game
